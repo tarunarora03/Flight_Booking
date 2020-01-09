87497d979e209e29875ec02f3f422fdc02d507cb
tarunarora03 <tarunarora03@gmail.com>
2020-01-09 00:10:11 -0500

    Config Server

---
+++ git-config-server/src/test/java/com/conf/GitConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
